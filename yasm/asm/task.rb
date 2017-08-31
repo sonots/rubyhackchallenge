187b7a295c11d9a7e6ee8b7b5401314778a9c18c
sonots <sonots@gmail.com>
2017-09-01 02:28:16 +0900

    task

--- yasm/asm/task.rb
+++ yasm/asm/task.rb
@@ -13,7 +13,7 @@ end
 
 # 1
 iseq = YASM.asm label: 'A-1: integer:1' do
-  putobject :replace_me
+  putobject 1
   leave
 end
 
@@ -21,7 +21,7 @@ assert iseq, 1
 
 # 1_000_000
 iseq = YASM.asm label: 'A-1: integer:1_000_000' do
-  putobject :replace_me
+  putobject 1_000_000
   leave
 end
 
@@ -29,7 +29,7 @@ assert iseq, 1_000_000
 
 # :ok
 iseq = YASM.asm label: "A-1': symbol:ok" do
-  putobject :replace_me
+  putobject :ok
   leave
 end
 
@@ -37,7 +37,7 @@ assert iseq, :ok
 
 # :ng
 iseq = YASM.asm label: "A-1': symbol:ng" do
-  putobject :replace_me
+  putobject :ng
   leave
 end
 
@@ -45,7 +45,7 @@ assert iseq, :ng
 
 # "hello"
 iseq = YASM.asm label: "A-1'': string:hello" do
-  putobject :replace_me
+  putobject "hello"
   leave
 end
 
@@ -53,7 +53,9 @@ assert iseq, "hello"
 
 # a = 1; a
 iseq = YASM.asm label: 'A-2: local_variables' do
-  putobject :replace_me
+  putobject 1
+  setlocal :a
+  getlocal :a
   leave
 end
 
@@ -61,7 +63,7 @@ assert iseq, 1
 
 # self
 iseq = YASM.asm label: 'A-3: self' do
-  putobject :replace_me
+  putself
   leave
 end
 
@@ -69,7 +71,7 @@ assert iseq, self
 
 # nil
 iseq = YASM.asm label: 'A-3: nil' do
-  putobject :replace_me
+  putnil
   leave
 end
 
@@ -77,7 +79,9 @@ assert iseq, nil
 
 # method call: 1 < 10 #=> true ( 1.<(10) #=> true )
 iseq = YASM.asm label: 'A-4: 1.<(10)' do
-  putobject :replace_me
+  putobject 1
+  putobject 10
+  send :<, 1
   leave
 end
 
@@ -85,7 +89,9 @@ assert iseq, true
 
 # method call: p(1) #=> 1
 iseq = YASM.asm label: 'A-4: p(1)' do
-  putobject :replace_me
+  putself
+  putobject 1
+  send :p, 1, YASM::FCALL
   leave
 end
 
@@ -93,7 +99,11 @@ assert iseq, 1
 
 # combination: 1 - 2 * 3 #=> -5
 iseq = YASM.asm label: "A-4': 1 - 2 * 3" do
-  putobject :replace_me
+  putobject 1
+  putobject 2
+  putobject 3
+  send :*, 1
+  send :-, 1
   leave
 end
 
@@ -101,7 +111,14 @@ assert iseq, -5
 
 # combination: a = 10; p(a > 1) #=> true
 iseq = YASM.asm label: "A-4': a = 10; p(a > 1)" do
-  putobject :replace_me
+  putobject 10
+  setlocal :a
+  putself
+  getlocal :a
+  putobject 1
+  send :>, 1
+  send :p, 1, YASM::FCALL
+
   leave
 end
 
@@ -109,7 +126,17 @@ assert iseq, true
 
 # combination: a = 1; b = 2; c = 3; a - b * c #=> -5
 iseq = YASM.asm label: "A-4': a = 1; b = 2; c = 3; a - b * c" do
-  putobject :replace_me
+  putobject 1
+  setlocal :a
+  putobject 2
+  setlocal :b
+  putobject 3
+  setlocal :c
+  getlocal :a
+  getlocal :b
+  getlocal :c
+  send :*, 1
+  send :-, 1
   leave
 end
 
@@ -117,39 +144,142 @@ assert iseq, -5
 
 # combination: p('foo'.upcase) #=> 'FOO'
 iseq = YASM.asm label: "A-4': p('foo'.upcase)" do
-  putobject :replace_me
+  putself
+  putobject 'foo'
+  send :upcase, 0
+  send :p, 1, YASM::FCALL
   leave
 end
 
 assert iseq, 'FOO'
 
 # if statement
+# a = 10
+# if a > 1
+#   p :ok
+# else
+#   p :ng
+# end
 iseq = YASM.asm label: 'A-5: if' do
-  putobject :replace_me
+  putobject 10
+  setlocal :a
+  getlocal :a
+  putobject 1
+  send :>, 1
+
+  branchunless :if_else
+
+  putself
+  putobject :ok
+  send :p, 1, YASM::FCALL
+
+  jump :if_end
+
+  label(:if_else)
+  # p(:ng)
+  putself
+  putobject :ng
+  send :p, 1, YASM::FCALL
+
+  label(:if_end)
   leave
 end
 
 assert iseq, :ok
 
 # if statement without else (1)
+# a = 10
+# if a > 1
+#   p :ok
+# end
 iseq = YASM.asm label: "A-5': if_without_else1" do
-  putobject :replace_me
+  putobject 10
+  setlocal :a
+  getlocal :a
+  putobject 1
+  send :>, 1
+
+  branchunless :if_end
+
+  putself
+  putobject :ok
+  send :p, 1, YASM::FCALL
+
+  label(:if_end)
   leave
 end
 
 assert iseq, :ok
 
 # if statement without else (2)
+# a = 10
+# if a < 1
+#   p :ok
+# end
 iseq = YASM.asm label: "A-5': if_without_else2" do
-  putobject :replace_me
+  putobject 10
+  setlocal :a
+  getlocal :a
+  putobject 1
+  send :<, 1
+
+  branchunless :if_else
+
+  putself
+  putobject :ok
+  send :p, 1, YASM::FCALL
+
+  jump :if_end
+
+  label(:if_else)
+  # nil
+  putnil
+
+  label(:if_end)
   leave
 end
 
 assert iseq, nil
 
 # while
+# a = 0
+# while (a < 10)
+#   p a
+#   a += 1
+# end #=> 10
 iseq = YASM.asm label: "A-6: while" do
-  putobject :replace_me
+  putobject 0
+  setlocal :a
+
+label :when_cond
+  # condition
+  getlocal :a
+  putobject 10
+  send :<, 1
+
+  # conditional jump
+  branchunless :when_end
+
+  # p a
+  putself
+  getlocal :a
+  send :p, 1, YASM::FCALL
+  pop
+
+  # a += 1 #=> a = a.+(1)
+
+  # a.+(1)
+  getlocal :a
+  putobject 1
+  send :+, 1
+
+  # a = ...
+  setlocal :a
+
+  jump :when_cond
+
+label :when_end
+  getlocal :a
   leave
 end
 
@@ -157,7 +287,10 @@ assert iseq, 10
 
 # def foo(); end
 iseq = YASM.asm label: "A-7: def:foo()" do
-  putobject :replace_me
+  define_method_macro :foo do
+    putnil
+    leave
+  end
   leave
 end
 
@@ -165,7 +298,16 @@ assert iseq, :foo
 
 # def foo(a); a; end; foo(100)
 iseq = YASM.asm label: 'A-7: def:foo(a)' do
-  putobject :replace_me
+  define_method_macro :foo, parameters: [:a] do
+    getlocal :a # as usual local variables
+    leave
+  end
+  pop
+
+  putself
+  putobject 100
+  send :foo, 1, YASM::FCALL
+
   leave
 end
 
